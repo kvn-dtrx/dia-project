e0f014579c4f67462fed4975200bcd6d62c07b3d
Kvn Dtrx <kvn.dtrx+git@protonmail.ch>
2025-12-08 02:58:27 +0100

    wip: Intermediate Commit @ 2025-12-08T02:58:27+01:00

--- src/dia_project/utils.py
+++ src/dia_project/utils.py
@@ -82,9 +82,7 @@ def fill_template(
             try:
                 with source.open("r", encoding="utf-8") as f:
                     contents.append(f.read())
-                logging.debug(
-                    f"Reading source file was successful:\n  {source}"
-                )
+                logging.debug(f"Reading source file was successful:\n  {source}")
             except Exception as e:
                 logging.error(f"Failed to read {source}: {e}")
         else:
